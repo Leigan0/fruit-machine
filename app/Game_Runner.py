9d95c782e519a867633f3742c1136289e63c7c0b
Leigh-ann <leighann.gant@gmail.com>
2018-04-22 18:10:37 +0100

    Spin reel method added to gamerunner - starts game deducts funds from player, spins reel

--- app/Game_Runner.py
+++ app/Game_Runner.py
@@ -6,3 +6,7 @@ class GameRunner:
     def __init__(self, machine=Machine, player=Player):
         self.player = player
         self.machine = machine
+
+    def spin_reel(self):
+        self.player.debit()
+        self.machine.spin_reel()
--- test/test_game_runner.py
+++ test/test_game_runner.py
@@ -7,7 +7,18 @@ class GameRunnerTestSpec(unittest.TestCase):
     def setUp(self):
         self.mock_machine = MagicMock()
         self.mock_player = MagicMock()
-        self.GameRunner = GameRunner(self.mock_machine, self.mock_player)
+        self.gamerunner = GameRunner(self.mock_machine, self.mock_player)
 
     def test_game_can_return_player(self):
-        self.assertEqual(self.GameRunner.player, self.mock_player)
+        self.assertEqual(self.gamerunner.player, self.mock_player)
+
+    def test_game_can_return_machine(self):
+        self.assertEqual(self.gamerunner.machine, self.mock_machine)
+
+    def test_machine_calls_spin_reel_method_on_machine_object(self):
+        self.gamerunner.spin_reel()
+        self.assertTrue(self.gamerunner.machine.spin_reel.called)
+
+    def test_machine_calls_debit_method_on_player_object(self):
+        self.gamerunner.spin_reel()
+        self.assertTrue(self.gamerunner.player.debit.called)
