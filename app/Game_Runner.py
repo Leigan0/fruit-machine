304f3160359347f3510ae240f13ecb5965988a35
Leigh-ann <leighann.gant@gmail.com>
2018-04-22 21:03:56 +0100

    Gamerunner spins reel, displays output, and reduces players fund balance

--- app/Game_Runner.py
+++ app/Game_Runner.py
@@ -4,11 +4,11 @@ from app.Printer import Printer
 
 class GameRunner:
 
-    def __init__(self, machine=Machine, player=Player, printer=Printer):
+    def __init__(self, machine, player, printer=Printer()):
         self.player = player
         self.machine = machine
         self.printer = printer
 
     def spin_reel(self):
-        self.player.debit()
-        self.machine.spin_reel()
+        self.player.debit(self.machine.MINIMUM_BET)
+        self.printer.print_display(self.machine.spin_reel())
